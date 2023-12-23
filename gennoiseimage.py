1ccbb16fc9d2e6202bc2024b9f0857c6438229ae
TooriG <97956577+TooriG@users.noreply.github.com>
2023-12-23 19:23:03 +0900

    Update gennoiseimage.py

--- gennoiseimage.py
+++ gennoiseimage.py
@@ -2,16 +2,18 @@ import streamlit as st
 import numpy as np
 from PIL import Image
 
-# Streamlitアプリのタイトル
-st.title('RGBランダムノイズ画像生成器')
+# Streamlitアプリのタイトルを設定
+st.title('RGB Random Noise Image Generator')
 
 # ユーザーがサイズを指定できるように入力を受け取る
-width = st.number_input('幅を入力してください', min_value=1, value=256)
-height = st.number_input('高さを入力してください', min_value=1, value=256)
+width = st.number_input('Enter width', min_value=1, value=256)
+height = st.number_input('Enter height', min_value=1, value=256)
 
-# ランダムノイズ画像を生成
-noise = np.random.randint(0, 256, (height, width, 3), dtype=np.uint8)
-image = Image.fromarray(noise)
+# 'Generate' ボタンを追加
+if st.button('Generate'):
+    # ランダムノイズ画像を生成
+    noise = np.random.randint(0, 256, (height, width, 3), dtype=np.uint8)
+    image = Image.fromarray(noise)
 
-# 画像を表示
-st.image(image, caption='ランダムRGBノイズ画像', use_column_width=True)
+    # 画像を表示
+    st.image(image, caption='Random RGB Noise Image', use_column_width=True)
